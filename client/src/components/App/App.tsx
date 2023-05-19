bc93fb0833f4b25eff491dd2037a5d05547ad003
Штас <stanislav.sinitsyn.work@gmail.com>
2023-05-19 20:08:48 +0300

    2

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,21 +1,19 @@
-
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NavBar1 from '../NavBar/NavBar';
+import NavBar1 from '../NavBar/NavBar1';
 import Reservation from '../Reservation/Reservation';
-import Layout from "../Layout/Layout";
+import Layout from '../Layout/Layout';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/admin/" element={<NavBar1 />}>
-          <Route path="reservation" element={<Reservation />} />
+          <Route path="reservation" element={<NavBar1 />} />
         </Route>
         <Route path="/" element={<Layout />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
