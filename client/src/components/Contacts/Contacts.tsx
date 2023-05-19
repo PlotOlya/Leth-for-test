424a16e7afc724d01c999118aa27d48ba456ce52
Olga Plotnikova <plotnikowa_olya@list.ru>
2023-05-19 15:30:23 +0300

    Map need API

--- client/src/components/Contacts/Contacts.tsx
+++ client/src/components/Contacts/Contacts.tsx
@@ -8,17 +8,13 @@ function Contacts(): JSX.Element {
     <section className="Contacts">
       <div className="contacts-container">
         <h2 className="adress">Адрес</h2>
-
         <div className="adress-items">
           <p>Набережная реки Фонтанки, 82</p>{" "}
           <p>5 минут от метро Звенигородская</p>
           <p>12:00 — 23:00</p>
         </div>
-
         <h2 className="phone">Связаться</h2>
-
         <div className="phone-item">Номер телефона: +7-904-648-60-60</div>
-
         <div className="social">
           <h2 className="">Социальные сети</h2>
 
@@ -34,8 +30,8 @@ function Contacts(): JSX.Element {
             <span className="social-item">Telegram</span>
           </a>
         </div>
+        <MapContakt />{" "}
       </div>
-      <MapContakt/>
     </section>
   );
 }
--- client/src/components/MapContact/MapContakt.tsx
+++ client/src/components/MapContact/MapContakt.tsx
@@ -3,18 +3,16 @@ import React, { memo } from "react";
 
 function MapContakt(): JSX.Element {
   return (
-    <div className="elementor-widget-container">
-      <div className="elementor-custom-embed">
-        Тут должна быть карта
-        <iframe
-          // frameBorder="0"
-          // scrolling="no"
-          // marginHeight="0"
-          // marginWidth="0"
-          src="https://maps.google.com/maps?q=%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%BD%D0%B0%D0%B1%D0%B5%D1%80%D0%B5%D0%B6%D0%BD%D0%B0%D1%8F%20%D1%80%D0%B5%D0%BA%D0%B8%20%D1%84%D0%BE%D0%BD%D1%82%D0%B0%D0%BD%D0%BA%D0%B8%2C%2082&amp;t=m&amp;z=14&amp;output=embed&amp;iwloc=near"
-          aria-label="Санкт-Петербург, набережная реки фонтанки, 82"
-        />
-      </div>
+    <div className="map-container">
+      Тут должна быть карта API
+      <iframe
+        // frameBorder="0"
+        // scrolling="no"
+        // marginHeight="0"
+        // marginWidth="0"
+        src="https://maps.google.com/maps?q=%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%BD%D0%B0%D0%B1%D0%B5%D1%80%D0%B5%D0%B6%D0%BD%D0%B0%D1%8F%20%D1%80%D0%B5%D0%BA%D0%B8%20%D1%84%D0%BE%D0%BD%D1%82%D0%B0%D0%BD%D0%BA%D0%B8%2C%2082&amp;t=m&amp;z=14&amp;output=embed&amp;iwloc=near"
+        aria-label="Санкт-Петербург, набережная реки фонтанки, 82"
+      />
     </div>
   );
 }
