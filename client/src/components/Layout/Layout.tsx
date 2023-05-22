e4e564e70300878ad9596860df361ca6bcd95784
Olga Plotnikova <plotnikowa_olya@list.ru>
2023-05-22 16:57:11 +0300

    Slider dane

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -5,6 +5,7 @@ import Footer from '../Footer/Footer'
 import Contacts from '../Contacts/Contacts'
 import Navbar from '../Navbar/Navbar'
 import VideoBlock from '../VideoBlock/VideoBlock'
+import Carusel from '../Carusel/Carusel'
 
 
 export default function Layout(): JSX.Element {
@@ -12,6 +13,7 @@ export default function Layout(): JSX.Element {
     <body className='body'>
         <Navbar />
         <VideoBlock />
+        <Carusel/>
         <Contacts/>
         <Footer/>
     </body>
