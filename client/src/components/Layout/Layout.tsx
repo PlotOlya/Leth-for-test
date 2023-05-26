b279020a533ad511dd795ff31f73c82c4673fd35
VanyaR <vanya.ruzhcov@gmail.com>
2023-05-26 10:30:51 +0300

    отработка материала

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Layout.css';
 import Footer from '../Footer/Footer';
-import Navbar from '../NavBar/NavBar';
+import Navbar from '../NavBar/Navbar';
 import Contacts from '../Contacts/Contacts';
 import VideoBlock from '../VideoBlock/VideoBlock';
 import Carusel from '../Carusel/Carusel';
-import DescriptionBlock from '../DescriptionBlock/DescriptionBlock';
+import DescriptionBlock from '../descriptionBlock/DescriptionBlock';
 import ShefBlock from '../ShefBlock/ShefBlock';
 import Menu from '../Menu/Menu';
 import Certificate from '../Certificate/CertificateForm';
